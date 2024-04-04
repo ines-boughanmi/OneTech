a3c910c875bc0dcde04639b8a59fc9a057bdd4f1
ElRayyMVM <rayenouelhezi@gmail.com>
2024-04-04 13:08:53 +0100

    popup

--- src/components/ParkingDashBoard/AddModal.js
+++ src/components/ParkingDashBoard/AddModal.js
@@ -3,8 +3,8 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import axios from "axios";
-import "./AddModal.css"
-import add from "../../assets/4211763.png"
+import "./AddModal.css";
+import add from "../../assets/4211763.png";
 
 const style = {
   position: "absolute",
@@ -19,38 +19,33 @@ const style = {
   borderRadius: "10px",
 };
 
-
-
 const AddModal = () => {
+  const [open, setOpen] = useState(false);
+  const [image, setImage] = useState("");
 
-    const [open, setOpen] = useState(false);
-    const [image, setImage] = useState("");
+  const handleOpen = () => {
+    setOpen(true);
+    setImage("");
+  };
+  const handleClose = () => setOpen(false);
 
-    const handleOpen = () =>{
-        setOpen(true);
-        setImage("")
-      }
-      const handleClose = () => setOpen(false);
-    
-
-
-    const profileUpload = async (e) => {
-        const formData = new FormData();
-        formData.append("file", e.target.files[0]);
-        formData.append("upload_preset", "oztadvnr");
-        await axios
-          .post("https://api.cloudinary.com/v1_1/dl4qexes8/upload", formData)
-          .then((response) => {
-            setImage(response.data["secure_url"]);
-          })
-          .catch((error) => {
-            throw error;
-          });
-      };
+  const profileUpload = async (e) => {
+    const formData = new FormData();
+    formData.append("file", e.target.files[0]);
+    formData.append("upload_preset", "oztadvnr");
+    await axios
+      .post("https://api.cloudinary.com/v1_1/dl4qexes8/upload", formData)
+      .then((response) => {
+        setImage(response.data["secure_url"]);
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
   return (
     <div>
       <Button className="button-add" onClick={handleOpen}>
-       + Add
+        + Add
       </Button>
       <Modal
         open={open}
@@ -71,43 +66,71 @@ const AddModal = () => {
                 </svg>
               </div>
               <div className="text">
-                <img src={image || add } alt="" className="addImage" />
+                <img src={image || add} alt="" className="addImage" />
               </div>
               <input type="file" id="file" onChange={(e) => profileUpload(e)} />
             </label>
             {/* End of custom file upload label */}
-            <input
-              type="text"
-              placeholder="First name"
-              className="textInputs"
-            />
-            <input type="text" placeholder="Last name" className="textInputs" />
-            <input type="text" placeholder="Email" className="textInputs" />
-            <div class="custom-select">
+
+            <div className="custom-select">
               <select>
-                <option value="" className="selectionFirst">
-                  Brand
+                <option disabled selected value="">
+                  Car Brand
                 </option>
+                <option value="Hyundai">Hyundai</option>
+                <option value="Citroen">Citroen</option>
+                <option value="Toyota">Toyota</option>
               </select>
             </div>
 
-            <div className="finalDate">
-              <p>Final Date :</p>
+            <input
+              type="text"
+              placeholder="Car Model Name"
+              className="textInputs"
+            />
+            <input
+              type="text"
+              placeholder="license plate Number"
+              className="textInputs"
+            />
+
+            <div className="custom-select">
+              <select>
+                <option disabled selected value="">
+                  Car Category
+                </option>
+                <option value="HatchBack">HatchBack</option>
+                <option value="SUV">SUV</option>
+                <option value="Truck">Truck</option>
+              </select>
+            </div>
+
+            <div className="custom-select">
+              <select placeholder="Color">
+                <option disabled selected value="">
+                  Car color
+                </option>
+                <option value="White">White</option>
+                <option value="Black">Black</option>
+                <option value="gray">gray</option>
+                <option value="silver">silver</option>
+                <option value="orange">orange</option>
+                <option value="blue">blue</option>
+              </select>
             </div>
             <input
-              type="date"
-              placeholder="Date"
-              className="dateInput"
-            />
-            
+            className="numberInputs"
+              type="number"
+              placeholder="Available Seats"
+              min="0"
+              max="10"
+            ></input>
+
             <div className="modalButtons">
               <Button className="modalBtn" onClick={handleClose}>
                 Cancel
               </Button>
-              <Button
-                className="modalBtn"  >
-                Add
-              </Button>
+              <Button className="modalBtn">Add</Button>
             </div>
           </div>
         </Box>
